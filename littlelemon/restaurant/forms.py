36074d20c0477ac87550b461b0ce94a442310c68
Abdullah Zubair <abdullahzubair2001@gmail.com>
2023-04-03 00:50:06 +0500

    first commit

---
+++ littlelemon/restaurant/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Booking
+
+
+# Code added for loading form data on the Booking page
+class BookingForm(ModelForm):
+    class Meta:
+        model = Booking
+        fields = "__all__"
+        # labels = [
+        #     "guest_number" = "Number of guests"
+        # ]
+        
